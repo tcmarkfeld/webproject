a28b84060b318f297198308c15149ca1ef0b0946
Kate Melvin <katemelvin@Kates-MacBook-Air.local>
2021-11-01 15:15:25 -0500

    webproject

---
+++ Client/admin/adminGardens/adminGarden.js
@@ -0,0 +1,28 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.4 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
---
+++ Client/landingPage/landingPage.js
@@ -0,0 +1,21 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.4 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// sticky nav bar
+// window.onscroll = function() {myFunction()};
+
+// var stickyNav = document.getElementById("stickyNav");
+// var sticky = stickyNav.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     stickyNav.classList.add("sticky")
+//   } else {
+//     stickyNav.classList.remove("sticky");
+//   }
+// }
