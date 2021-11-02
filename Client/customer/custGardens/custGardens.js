c96caa5a5df8494d117a705fc0fac5d3017bc405
Timothy Markfeld <tcmarkfeld@crimson.ua.edu>
2021-11-02 13:45:49 -0500

    Updates

--- Client/customer/custGardens/custGardens.js
+++ Client/customer/custGardens/custGardens.js
@@ -5,18 +5,49 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+const GetGarden = async (id) => {
+  const gardenURL = `https://localhost:5001/api/garden/${id}`;
+  const response = await fetch(gardenURL);
+  const data = await response.json();
+  return data;
+}
 
 // Get the modal
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
+var img = document.getElementById('myImg1'); // House plant garden modal
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
+  var data = GetGarden(1).then(displayDataInCaption(data))
+}
+var img = document.getElementById('myImg2'); // Outdoor garden modal
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = async function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  var data = await GetGarden(2).then((data)=>displayDataInCaption(data)).then(console.log(data));
+}
+var img = document.getElementById('myImg3'); //Industrial garden modal
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  var data = GetGarden(3).then((data)=>displayDataInCaption(data))
+}
+
+const displayDataInCaption = (data) =>{
+  var info = document.getElementById("info");
+  var html = `${data.information}`
+  info.innerHTML = html;
 }
 
 // Get the <span> element that closes the modal
