5a38931f3abaa9cf05c18ccb267dc7c6b8b9be9a
Timothy Markfeld <tcmarkfeld@crimson.ua.edu>
2021-11-23 15:15:38 -0500

    Updates

--- Client/admin/adminGardens/adminGarden.js
+++ Client/admin/adminGardens/adminGarden.js
@@ -9,15 +9,13 @@ const GetGarden = async (id) => {
 function displayModal(data){
   console.log(data);
   var garden = document.getElementById("exampleModal");
-  var html = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content">`;
+  var html = `<div class="modal-dialog"><div class="modal-content">`;
   html += `<div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">${data[0].gardenType}</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>`;
-  html += `</div><div class="modal-body"><img id="myImg" src="./assets/garden-${data[0].gardenID}.jpg" alt="House Plants" width="300" height="200"/><div id="gardeninfo">${data[0].information}</div>`;
+  html += `</div><div class="modal-body"><img class="modalImg" src="./assets/garden-${data[0].gardenID}.jpg" alt="House Plants" width="300" height="200"/><div id="gardeninfo">${data[0].information}</div>`;
   html += `</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Save changes</button>`;
-  html += `</div></div></div></div>`;
+  html += `</div></div></div>`;
   garden.innerHTML = html;
-  //$('.modal-backdrop').remove();
 }
-
 
 var myModal = new bootstrap.Modal(document.getElementById('exampleModal'))
 
