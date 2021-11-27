7fd8453fbf4234fb33babbeead5d05c508061788
jwcarter-us <jwcarter@crimson.ua.edu>
2021-11-26 22:36:26 -0600

    Adding Register Function

--- Client/customer/custRegister/custRegister.js
+++ Client/customer/custRegister/custRegister.js
@@ -5,3 +5,43 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function registerAccount(){
+    const customerApiUrl = "https://localhost:5001/api/customer";
+    const firstname = document.getElementById(`fname`);
+    const lastname = document.getElementById(`lname`);
+    const birthdate = document.getElementById(`birthdate`);
+    const email2 = document.getElementById(`email`);
+    const password2 = document.getElementById(`password`);
+    if(document.getElementById("subscribeToEmails").checked==true){
+        const status="yes";
+    }
+    else{
+        const status="no";
+    }
+    if(document.getElementById("termsAndCond").checked==true){
+        fetch(customerApiUrl, {
+            method: "POST",
+            headers: {
+                "Accept": 'application/json',
+                "Content-Type": 'application/json'
+    
+            },
+            body: JSON.stringify({
+                firstName: firstname,
+                lastName: lastname,
+                birthdate: birthdate,
+                email: email2,
+                password: password2,
+            })
+        })
+        .then((response)=>{
+            console.log(response);
+        })
+    }
+    else{
+        var errorMessage = document.getElementById("errorMsg");
+        var html = "You must agree to the Terms and Conditions before registering your account.";
+        errorMessage.innerHTML=html;
+    }
+}
